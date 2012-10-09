566e19008fcbe673bdcb424a925533ae67a9fea2
Alice Hubenko <alice@eucalyptus.com>
2012-10-08 17:05:57 -0700

    Setup testing environment, install eutester

--- install.py
+++ install.py
@@ -13,53 +13,53 @@ subprocess.call('rm -f eutester_install.log', shell=True)
 
 def main():
 
-    cmd="yum -y install python-virtualenv"
+    cmd="cd /root/ ; yum install python-setuptools"
     pcmd(cmd)
 
-    cmd="cd /root/ &&  mkdir virtual_env"
+    cmd="cd /root/ ; easy_install virtualenv"
     pcmd(cmd)
 
-    cmd="cd /root/ &&  virtualenv virtual_env"
+    cmd="cd /root/ ;  mkdir virtual_env"
     pcmd(cmd)
 
-    cmd="cd /root/virtual_env/bin && source activate"
+    cmd="cd /root/virtual_env ; virtualenv virtual_env"
     pcmd(cmd)
 
-    cmd="cd /root/ && wget http://argparse.googlecode.com/files/argparse-1.2.1.tar.gz -O argparse.tar.gz && tar -zxvf argparse.tar.gz"
+    cmd="cd /root/virtual_env/bin ; source activate"
     pcmd(cmd)
 
-    cmd="cd /root/argparse-1.2.1 && python setup.py install"
+    cmd="cd /root/ ; wget http://argparse.googlecode.com/files/argparse-1.2.1.tar.gz -O argparse.tar.gz && tar -zxvf argparse.tar.gz"
     pcmd(cmd)
 
-    cmd="cd /root/ && wget http://192.168.51.136/deps/boto-2.5.2.tar.gz -O boto.tgz && tar -zxvf boto.tgz"
+    cmd="cd /root/argparse-1.2.1 ; python setup.py install"
     pcmd(cmd)
 
-    cmd="cd /root/boto-2.5.2 && python setup.py install"
+    cmd="cd /root/ ; easy_install boto==2.5.2"
     pcmd(cmd)
 
-    cmd="cd /root/ && yum -y install git gcc python-paramiko python-devel"
+    cmd="cd /root/ ; yum -y install git gcc python-paramiko python-devel"
     pcmd(cmd)
 
-    cmd="cd /root/ && git clone https://github.com/eucalyptus/eutester.git"
+    cmd="cd /root/ ; git clone https://github.com/eucalyptus/eutester.git"
     pcmd(cmd)
 
-    cmd="cd /root/eutester && git checkout testing"
+    cmd="cd /root/eutester ; git checkout testing"
     pcmd(cmd)
 
-    cmd="cd /root/eutester && python ./setup.py install"
+    cmd="cd /root/eutester ; python ./setup.py install"
     pcmd(cmd)
 
-    cmd="cd /root/ && mkdir testerworkdir"
+    cmd="cd /root/ ; mkdir testerworkdir"
     pcmd(cmd)
 
-    cmd="cd /root/virtual_env/bin && source activate"
+    cmd="cd /root/virtual_env/bin ; source activate"
     pcmd(cmd)
 
 
-    cmd="cd /root/ && yum install ntp"
+    cmd="cd /root/ ; yum install ntp"
     pcmd(cmd)
 
-    cmd="chkconfig ntpd on && service ntpd start && ntpdate -u 0.centos.pool.ntp.org"
+    cmd="chkconfig ntpd on ; service ntpd start && ntpdate -u 0.centos.pool.ntp.org"
     pcmd(cmd)
 
     # to extract ntp server address from ntp.conf file:  
