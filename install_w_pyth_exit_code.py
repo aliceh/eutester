32ec673d72fc311db0a5c4bbeae030579e5b436b
Alice Hubenko <alice@eucalyptus.com>
2012-10-15 16:10:39 -0700

    Setup testing environment, install eutester, create  a file containing long log of the install and a file containing short log of the install with exit codes.  Log files are created via python calls.

--- install_w_pyth_exit_code.py
+++ install_w_pyth_exit_code.py
@@ -1,88 +1,88 @@
 #!/usr/bin/python
-#This code prepares eutester environment and outputs install log info and exit code info into two files.
-#Writing of the log info and exit code info is executed through python (not shell) calls
+#This code prepares eutester environment and outputs install log inlogo and exit code inlogo into two logiles.
+#Writing olog the log inlogo and exit code inlogo is executed through python (not shell) calls
 
 import sys
 import subprocess
 
 
-def pcmd(cmd):
+def print_command(shell_command):
 
 
-    comstr = '\nCOMMAND :' + cmd + '\n'
-    print comstr
-    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
-    output = p.stdout.read()
-    retcode = str(p.wait())
-    f.write(comstr + output)
-    f1.write(comstr+ 'Exitcode: '+retcode )
-subprocess.call('rm -f install_log.txt', shell=True)
-subprocess.call('rm -f install_exit_code.txt', shell=True)
+    command_string = '\nCOMMAND :' + shell_command + '\n'
+    print command_string
 
-workmain = "/root/" #main working directory
-#output = open("eutester_output.txt", "w")
-#error = open("eutester_error.txt", "w")
-f = open("install_log.txt", "w")
-f1 = open("install_exit_code.txt", "w")
+    execute_shell_command = subprocess.Popen(shell_command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+    output =  execute_shell_command.stdout.read()
+    exit_code = str( execute_shell_command.wait())
+    log.write(command_string + output)
+    log_short.write(command_string+ 'Exitcode: '+exit_code )
+    subprocess.call('rm -log install_log.txt', shell=True)
+    subprocess.call('rm -log install_exit_code.txt', shell=True)
+
+    workmain = "/root/" #main working directory
+
+    log = open("install_log.txt", "w")
+    log_short = open("install_exit_code.txt", "w")
 while (sys.version_info < (2, 6)):
-    print "Found python version "+str(sys.version_info[0])+"."+str(sys.version_info[1])+ " must use python 2.6 or greater."
+    print "found python version "+str(sys.version_info[0])+"."+str(sys.version_info[1])+ " must use python 2.6 or greater."
     sys.exit()
 
 
 def main():
 
-    cmd="cd "+ workmain + " ;" +" yum -y install python-setuptools"
-    pcmd(cmd)
+    shell_command="cd "+ workmain + " ;" +" yum -y install python-setuptools"
+    print_command(shell_command)
 
-    cmd="cd "+ workmain +";"+" easy_install virtualenv"
-    pcmd(cmd)
+    shell_command="cd "+ workmain +";"+" easy_install virtualenv"
+    print_command(shell_command)
 
-    cmd="cd "+ workmain +";"+" virtualenv virtual_env"
-    pcmd(cmd)
+    shell_command="cd "+ workmain +";"+" virtualenv virtual_env"
+    print_command(shell_command)
 
-    cmd="bash -c 'cd "+ workmain +"virtual_env/bin; source activate'"
+    shell_command="bash -c 'cd "+ workmain +"virtual_env/bin; source activate'"
     #; echo exit code $?; echo prompt $PS1 '"
-    pcmd(cmd)
+    print_command(shell_command)
 
 
-    cmd="cd "+ workmain +";"+" wget http://argparse.googlecode.com/files/argparse-1.2.1.tar.gz -O argparse.tar.gz && tar -zxvf argparse.tar.gz"
-    pcmd(cmd)
+    shell_command="cd "+ workmain +";"+" wget http://argparse.googlecode.com/logiles/argparse-1.2.1.tar.gz -O argparse.tar.gz && tar -zxvlog argparse.tar.gz"
+    print_command(shell_command)
 
-    cmd="cd "+ workmain +"argparse-1.2.1 ; python setup.py install"
-    pcmd(cmd)
+    shell_command="cd "+ workmain +"argparse-1.2.1 ; python setup.py install"
+    print_command(shell_command)
 
-    cmd="cd "+ workmain +";"+" easy_install boto==2.5.2"
-    pcmd(cmd)
+    shell_command="cd "+ workmain +";"+" easy_install boto==2.5.2"
+    print_command(shell_command)
 
-    cmd="cd "+ workmain +";"+" yum -y install git gcc python-paramiko python-devel"
-    pcmd(cmd)
+    shell_command="cd "+ workmain +";"+" yum -y install git gcc python-paramiko python-devel"
+    print_command(shell_command)
 
-    cmd="cd "+ workmain +";"+" git clone https://github.com/eucalyptus/eutester.git"
-    pcmd(cmd)
+    shell_command="cd "+ workmain +";"+" git clone https://github.com/eucalyptus/eutester.git"
+    print_command(shell_command)
 
-    cmd="cd " + workmain +"eutester ; git checkout testing"
-    pcmd(cmd)
+    shell_command="cd " + workmain +"eutester ; git checkout testing"
+    print_command(shell_command)
 
-    cmd="cd " + workmain +"eutester ; python ./setup.py install"
-    pcmd(cmd)
+    shell_command="cd " + workmain +"eutester ; python ./setup.py install"
+    print_command(shell_command)
 
-    cmd="cd "+ workmain +";"+" mkdir testerworkdir"
-    pcmd(cmd)
+    shell_command="cd "+ workmain +";"+" mkdir testerworkdir"
+    print_command(shell_command)
 
-    cmd="cd "+ workmain +";"+" yum -y install ntp"
-    pcmd(cmd)
+    shell_command="cd "+ workmain +";"+" yum -y install ntp"
+    print_command(shell_command)
 
-    cmd="chkconfig ntpd on ; service ntpd start && ntpdate -u 0.centos.pool.ntp.org"
-    pcmd(cmd)
+    shell_command="chkconlogig ntpd on ; service ntpd start && ntpdate -u 0.centos.pool.ntp.org"
+    print_command(shell_command)
 
-    # to extract ntp server address from ntp.conf file:  
-    # cat /etc/ntp.conf|grep "server 0.*pool.ntp.org" |awk '{print $2}'
+    # to extract ntp server address logrom ntp.conlog logile:  
+    # cat /etc/ntp.conlog|grep "server 0.*pool.ntp.org" |awk '{print $2}'
 
-    cmd="ntpdate -u 0.centos.pool.ntp.org"
-    pcmd(cmd)
+    shell_command="ntpdate -u 0.centos.pool.ntp.org"
+    print_command(shell_command)
 
-    f.close()
-    f1.close()
+    log.close()
+    log_short.close()
 
 if __name__ == '__main__':
     main()
